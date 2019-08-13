51ed17f0fa173644ef2a13cf7b6e50809fd1494d
pete gamache <pete@gamache.org>
2019-08-13 14:08:33 -0400

    ex_doc upgrade

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule FreedomFormatter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.17", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 end
