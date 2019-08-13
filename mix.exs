674ce2f37504d6b4476e28a4b28f13aeeea930da
pete gamache <pete@gamache.org>
2019-08-13 13:22:01 -0400

    1.1.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FreedomFormatter.MixProject do
     [
       app: :freedom_formatter,
       description: "A fork of the Elixir code formatter, with added freedom",
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
