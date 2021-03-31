c3cfc20f24dd1c11f8e018c2c90d2f714932bdcf
Marc-Andre Lafortune <github@marc-andre.ca>
2021-03-30 21:22:26 -0400

    Support for trailing comma

--- lib/freedom_formatter/formatter.ex
+++ lib/freedom_formatter/formatter.ex
@@ -198,6 +198,7 @@ defmodule FreedomFormatter.Formatter do
       locals_without_parens: locals_without_parens ++ locals_without_parens(),
       operand_nesting: 2,
       skip_eol: false,
+      trailing_comma: Keyword.get(opts, :trailing_comma, false),
       comments: comments,
       sigils: sigils
     }
@@ -1435,7 +1436,7 @@ defmodule FreedomFormatter.Formatter do
     fun = &quoted_to_algebra(&1, :parens_arg, &2)
 
     {args_doc, _join, state} =
-      args_to_algebra_with_comments(args, meta, false, :none, join, state, fun)
+      args_to_algebra_with_comments(args, meta, false, :state, join, state, fun)
 
     {surround("[", args_doc, "]"), state}
   end
@@ -1446,7 +1447,7 @@ defmodule FreedomFormatter.Formatter do
     {left_doc, state} = fun.(left, state)
 
     {right_doc, _join, state} =
-      args_to_algebra_with_comments(right, meta, false, :none, join, state, fun)
+      args_to_algebra_with_comments(right, meta, false, :state, join, state, fun)
 
     args_doc =
       left_doc
@@ -1462,7 +1463,7 @@ defmodule FreedomFormatter.Formatter do
     fun = &quoted_to_algebra(&1, :parens_arg, &2)
 
     {args_doc, _join, state} =
-      args_to_algebra_with_comments(args, meta, false, :none, join, state, fun)
+      args_to_algebra_with_comments(args, meta, false, :state, join, state, fun)
 
     name_doc = "%" |> concat(name_doc) |> concat("{")
     {surround(name_doc, args_doc, "}"), state}
@@ -1594,12 +1595,29 @@ defmodule FreedomFormatter.Formatter do
     arg_to_algebra = fn arg, args, state ->
       {doc, state} = fun.(arg, state)
 
-      doc =
+      {doc, state} =
         case args do
-          [_ | _] -> concat_to_last_group(doc, ",")
-          [] when last_arg_mode == :force_comma -> concat_to_last_group(doc, ",")
-          [] when last_arg_mode == :next_break_fits -> next_break_fits(doc, :enabled)
-          [] when last_arg_mode == :none -> doc
+          [{:|, _, _}] ->
+            {concat_to_last_group(doc, ","), Map.put(state, :trailing_cons, true)}
+
+          [_ | _] ->
+            {concat_to_last_group(doc, ","), state}
+
+          [] when last_arg_mode == :force_comma ->
+            {concat_to_last_group(doc, ","), state}
+
+          [] when last_arg_mode == :next_break_fits ->
+            {next_break_fits(doc, :enabled), state}
+
+          [] when last_arg_mode == :none ->
+            {doc, state}
+
+          [] when last_arg_mode == :state ->
+            if !Map.get(state, :trailing_cons) && state.trailing_comma && join == :line do
+              {concat_to_last_group(doc, ","), state}
+            else
+              {doc, state}
+            end
         end
 
       {{doc, @empty, 1}, state}
