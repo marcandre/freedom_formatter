89cc2057b1843e7f2ca4a62a5f866a26dd1784b9
pete gamache <pete@gamache.org>
2021-03-30 20:36:01 -0400

    Initial setup

---
+++ .formatter.exs
@@ -0,0 +1,12 @@
+# These parameters should not deviate from the Elixir .formatter.exs,
+# so that merges are easy
+[
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    # Formatter tests
+    assert_format: 2,
+    assert_format: 3,
+    assert_same: 1,
+    assert_same: 2
+  ]
+]
---
+++ mix.exs
@@ -0,0 +1,40 @@
+defmodule FreedomFormatter.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :freedom_formatter,
+      description: "A fork of the Elixir code formatter, with added freedom",
+      version: "1.0.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      aliases: [
+        docs: "docs --source-url https://github.com/gamache/freedom_formatter"
+      ]
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["Apache 2.0"],
+      maintainers: ["pete gamache <pete@gamache.org>"],
+      links: %{github: "https://github.com/gamache/freedom_formatter"}
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+end
